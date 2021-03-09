e457ee7bec63e3df02753f3fe0c8dfa5a5899829
Jonas Riedel <jonasriedel@gmx.net>
2021-03-09 20:57:12 +0100

    ADD: implemented valid height and weight check

--- script.js
+++ script.js
@@ -9,6 +9,24 @@ function calculate() {
     bmi = Math.round(weight / (Math.pow((height / 100), 2)));
     bmiSpan.innerText = 'Dein BMI beträgt: ' + bmi;
 
+    if (weight < 10 || weight > 600) {
+        swal('Ungültiges Gewicht. Bitte Daten erneut eingeben');
+
+        heightInput.value = '';
+        weightInput.value = '';
+
+        return;
+    }
+
+    if (height < 50 || height > 400) {
+        swal('Ungültige Größe. Bitte Daten erneut eingeben');
+
+        heightInput.value = '';
+        weightInput.value = '';
+
+        return;
+    }
+
     heightInput.value = '';
     weightInput.value = '';
 }
